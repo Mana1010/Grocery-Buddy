cea7b3a77161bf79095592417f3e1cd1d40c3490
MANA <108984739+Mana1010@users.noreply.github.com>
2023-07-05 15:44:43 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import GroceryBuggy from './GroceryBuggy'
+import Beshy from './Beshy'
+
+function App() {
+  const [count, setCount ] = useState(0);
+  return (
+    <>
+    <Beshy/>
+    </>
+  )
+
+}
+
+export default App
+
+
